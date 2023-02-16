3125e830048fc3bbceaf49864240e1b7b83e8369
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2023-02-16 23:16:23 +0800

    Mapper and reducer for word count

--- CodeAssignment_1/assign1_common_words/TopkCommonWords.java
+++ CodeAssignment_1/assign1_common_words/TopkCommonWords.java
@@ -1,11 +1,74 @@
 /*
-ENTER YOUR NAME HERE
-NAME: JOHN DOE
-MATRICULATION NUMBER: A0123456X
+NAME: Papattarada Apithanangsiri
+MATRICULATION NUMBER: A0222677Y
 */
 
-public class TopkCommonWords {
-    public static void main(String[] args){
+import java.io.IOException;
+import java.util.StringTokenizer;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
-    } 
+public class TopkCommonWords {
+
+  // mapper: tokenize words from file
+  public static class TokenizerMapper
+    extends Mapper<Object, Text, Text, IntWritable> {
+    private static final IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+
+    @Override
+    public void map(Object key, Text value, Context con)
+      throws IOException, InterruptedException {
+      StringTokenizer itr = new StringTokenizer(value.toString());
+
+      while (itr.hasMoreTokens()) {
+        word.set(itr.nextToken());
+        con.write(word, one);
+      }
+    }
+  }
+
+  // reducer: count each word
+  public static class IntSumReducer
+    extends Reducer<Text, IntWritable, Text, IntWritable> {
+    private IntWritable wordCount = new IntWritable();
+
+    @Override
+    public void reduce(Text key, Iterable<IntWritable> values, Context con)
+      throws IOException, InterruptedException {
+      int sum = 0;
+
+      for (IntWritable value : values) {
+        sum += value.get();
+      }
+
+      wordCount.set(sum);
+      con.write(key, wordCount);
+    }
+  }
+
+  // driver
+  public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+    Job job = Job.getInstance(conf, "top k words");
+    job.setJarByClass(TopkCommonWords.class);
+    job.setMapperClass(TokenizerMapper.class);
+    job.setReducerClass(IntSumReducer.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileInputFormat.addInputPath(job, new Path(args[1]));
+
+    FileOutputFormat.setOutputPath(job, new Path(args[3]));
+
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
+  }
 }
